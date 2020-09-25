a0476286a03958947fd4e4ad0d1b206f7387d079
pablo.gonzalezg <pablo.gonzalez.granados@gmail.com>
2020-09-25 16:10:54 +0200

    docker image name is git short commit

--- src/main/docker/build.env.sh
+++ src/main/docker/build.env.sh
@@ -4,15 +4,7 @@ export GIT_TAG=$(git describe --abbrev=0 --tags 2> /dev/null)
 export GIT_BRANCH=$(basename $(git branch -r --contains HEAD))
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 export GIT_COMMIT_SHORT=$(git rev-parse HEAD)
-
-if [ -z $GIT_TAG ]
-then
-    export BASE_VERSION=$GIT_COMMIT;
-else
-    export BASE_VERSION=$GIT_TAG
-fi
-
-export VERSION=$BASE_VERSION
+export VERSION=$GIT_COMMIT
 
 export DOCKER_IMAGE="pjgg/code-with-quarkus:$VERSION"
 
