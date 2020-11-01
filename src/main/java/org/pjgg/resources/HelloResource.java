30418a90a1ed78406216a9f61c66c04026931f1a
Pablo José González Granados <pablojosegonzalezgranados@localhost.localdomain>
2020-11-01 12:39:18 +0100

    Add custom service executor example

--- src/main/java/org/pjgg/resources/HelloResource.java
+++ src/main/java/org/pjgg/resources/HelloResource.java
@@ -1,7 +1,11 @@
 package org.pjgg.resources;
 
+import io.smallrye.context.api.NamedInstance;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,9 +17,19 @@ public class HelloResource {
     @ConfigProperty(name = "org.pjgg.greeting")
     private String greeting;
 
+    @Inject
+    @NamedInstance("customServiceExecutor")
+    ManagedExecutor delorean;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        System.out.println(java.lang.Thread.activeCount());
+
+        var promise = delorean.supplyAsync(()-> "You will see this in the future !!!");
+        promise.thenAccept(System.out::println);
+        System.out.println(java.lang.Thread.activeCount());
+
         return greeting;
     }
 }
